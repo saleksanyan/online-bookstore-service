04086c5fe725cd502f861e8778eb33430d2b0a3e
saleksanyan <92786852+saleksanyan@users.noreply.github.com>
2024-05-07 15:18:29 +0400

    Update main.ts

--- scr/main.ts
+++ scr/main.ts
@@ -1,7 +1,1 @@
-// This TypeScript file intentionally introduces an ESLint error
 
-// Declare a variable without using it
-const unusedVariable: string = 'Hello'
-
-// Log a message to the console
-console.log('This is a deliberate ESLint error example')
